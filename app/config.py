6d3ca9a8f78b13e39ab1fbb4755f5a4a77a75334
ImranBabu <bimran357@gmail.com>
2020-09-25 10:23:53 -0700

    Day 3 - Templates

---
+++ app/config.py
@@ -0,0 +1,2 @@
+class Config(object):
+    SECRET_KEY = "Module_not_Found"
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "This is Index Page"
+    user = {"username" : "Imran"}
+    a = ["Imran","Muzamil","Surya"]
+    return render_template("index.html",title = "Imran's Flask ",user = user,ex_list = a)
