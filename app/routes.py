79e4b50562891154d82cac23ea4b143562ce1927
ImranBabu <bimran357@gmail.com>
2020-09-29 10:35:19 -0700

    Added login backend data

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,7 @@ from flask import render_template,flash,redirect,url_for
 from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index1')
+@app.route('/index')
 def index():
     user = {"username" : "Imran"}
     a = ["Imran","Muzamil","Surya"]
