dcabe565a26f462160bcc464a5057314789c95cf
ImranBabu <bimran357@gmail.com>
2020-09-29 10:31:24 -0700

    Login form data retriving backend

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
 from app import app
-from flask import render_template
+from flask import render_template,flash,redirect,url_for
 from app.forms import LoginForm
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index1')
 def index():
     user = {"username" : "Imran"}
     a = ["Imran","Muzamil","Surya"]
     return render_template("index.html",title = "Imran's Flask ",user = user,ex_list = a)
 
-@app.route('/login')
+@app.route('/login',methods = ["GET","POST"])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash("Login Requested for User {} , remember me = {}".format(form.uname.data,form.remember_me.data))
+        return redirect(url_for("index"))
     return render_template("login.html",title = "Sign In",form = form)
